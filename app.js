536b8d20f4ebe9115519e9ccabe214bb5fcc2de2
Cameron Weston <cameronbweston@gmail.com>
2021-06-25 13:38:31 -0500

    Begin writing render function

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const game = {
     smallestNum: 1,
     secretNum: null,
     prevGuesses: [],
+    play: function() {
+      this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    },
     getGuess: function() {
       let guess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`);
       this.prevGuesses.push(guess);
     },
-    play: function() {
-      this.secretNum = Math.floor(Math.random() * 
-        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    render: function() {
+      if (this.prevGuesses.length != 0) { //Make sure array is not empty
+        latestGuess = this.prevGuesses[this.prevGuesses.length - 1];
+        if (latestGuess > this.secretNum) {
+          alert('Guess lower!');
+        }
+        else if (latestGuess < this.secretNum) {
+          alert('Guess higher!');
+        }
+        else {
+          //WON THE GAME
+        }
+      }
     }
   }
 
@@ -19,3 +33,5 @@ const game = {
   //3. Maintain list of all previously guessed numbers
   //4. If player guesses correctly, display alert with congratulations and how many guesses it took
 
+game.play();
+game.getGuess();
